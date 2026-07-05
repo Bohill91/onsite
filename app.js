b202ba35a1e9d6a0d4c14ce012132106444e6e78
Luke Curtis Bohill <lukecurtisbohill@Lukes-MacBook-Air.local>
2026-07-05 17:36:53 +0100

    Improve Request Labour map styling

--- app.js
+++ app.js
@@ -10871,10 +10871,11 @@ function initPickerMap() {
     [52.4862, -1.8904],
     11,
   );
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
     attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 19,
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    maxZoom: 20,
+    subdomains: "abcd",
   }).addTo(pickerMap);
 
   const pinIcon = L.divIcon({
